79deea86fa5f155f27dd3b859e26365df8d75708
Bryan Rosales <bryanrosales@Bryans-MacBook-Pro.local>
2015-02-20 08:20:09 -0600

    Pagina de login signup services and promotions

---
+++ app/src/main/java/com/example/bryanrosales/zentre/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bryanrosales.zentre;
+
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+
+
+public class MainActivity extends Activity
+
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Send_SingUpForm(View view)
+    {
+        Intent intent = new Intent(this, SingUp_Activity.class);
+        startActivity(intent);
+    }
+
+    public void Send_Home(View view)
+    {
+        Intent intent = new Intent(this, TabsActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/bryanrosales/zentre/TabsActivity.java
@@ -0,0 +1,68 @@
+package com.example.bryanrosales.zentre;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
+
+
+public class TabsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabs);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost2);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("services");
+        tabSpec.setContent(R.id.linearLayout);
+        tabSpec.setIndicator("Services");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("userinfo");
+        tabSpec.setContent(R.id.linearLayout2);
+        tabSpec.setIndicator("UserInfo");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("promotions");
+        tabSpec.setContent(R.id.linearLayout3);
+        tabSpec.setIndicator("Promotions");
+        tabHost.addTab(tabSpec);
+    }
+
+    public void Send_Dialog_Restaurant(View view){
+        Intent intent = new Intent(this, Dialog_Rest_1.class);
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tabs, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
